4e978828875bcf5cc6fb491e768ad54f5147079f
Berk Sudan <berk.sudan.96@gmail.com>
2017-11-03 19:43:04 +0300

    Update createLauncher.sh

--- createLauncher.sh
+++ createLauncher.sh
@@ -4,23 +4,27 @@ printf "Let's create a desktop launcher of your app:\n"
 printf "What is your app's command?\n"
 
 read app_name
-touch ~/Desktop/$app_name\ Launcher.desktop
+appropriateName=${app_name##*/}
 
-printf "
-#!/usr/bin/env xdg-open
+ 
+touch ~/Desktop/$appropriateName.desktop
+
+echo "Created: ~/Desktop/$appropriateName"
+
+printf "#!/usr/bin/env xdg-open
 [Desktop Entry]
 Version=1.0
 Type=Application
 Terminal=false
-Icon[en_US]=$app_name
 Exec=$app_name
-Name[en_US]=$app_name
-Comment[en_US]=$app_name
-Name=$app_name
-Comment=$app_name
-Icon=$app_name
-" | tee  ~/Desktop/$app_name\ Launcher.desktop
+Name[en_US]=$appropriateName
+Comment[en_US]="Launcher\ \for\ $appropriateName"
+Icon[en_US]=""
+Name=$appropriateName
+Comment="Launcher\ \for\ $appropriateName"
+Icon=""
+" | tee  ~/Desktop/$appropriateName.desktop
 
-sudo chmod +x ~/Desktop/$app_name\ Launcher.desktop 
+sudo chmod +x ~/Desktop/$appropriateName.desktop
 
 printf "\nYour app launcher is ready in ~/Desktop\n" 
