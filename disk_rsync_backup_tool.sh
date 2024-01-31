35a14011549534896e5843e996b52c318380be13
bs <23002942+berksudan@users.noreply.github.com>
2024-01-31 08:52:35 +0100

    dry run added to the rsync as a safety

--- disk_rsync_backup_tool.sh
+++ disk_rsync_backup_tool.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ## How to Use:
-##	1. Place this file into your backup disk (the disk you will use as a backup)
-##	2. Set DEF_SOURCE_DIR path as your main disk (the disk which contains the files needed to backup) 
-##	3. Execute this bash file in Linux Terminal.
-##	4. Wait until the synchronization process ends. The backup disk may be damaged in case of some interruption during transfer.
+##  1. Place this file into your backup disk (the disk you will use as a backup)
+##  2. Set DEF_SOURCE_DIR path as your main disk (the disk which contains the files needed to backup)
+##  3. Execute this bash file in Linux Terminal.
+##  4. Wait until the synchronization process ends. The backup disk may be damaged in case of some interruption during transfer.
 
 DEF_SOURCE_DIR="/media/${USER}/Seagate Expansion Drive/"
 
@@ -36,4 +36,25 @@ read -p "Is it the content of the source directory? [Y/n]: " yn
     esac
 
 backup_bash_filename=`basename "$0"`
-rsync -avP  "$source_dir" "./" --exclude $backup_bash_filename --delete
+
+echo "[INFO] You will see a DRY RUN out of rsync, in 3 seconds.."
+sleep 3
+rsync --dry-run -avhP --delete \
+    --exclude $backup_bash_filename \
+    --exclude '$RECYCLE.BIN/' \
+    --exclude 'System Volume Information' \
+    --exclude '.Trash-1000' \
+    "$source_dir" "./" | grep --color=always "deleting\|$"
+
+echo "-----------------------------"
+
+read -p "Are you satisfied with the DRY RUN and want to do an actual rsync? [y/N]:" yn
+    case $yn in
+        N|n|"") echo "exiting.."; exit 1 ;;
+        Y|y|*) rsync -avhP --delete \
+    --exclude $backup_bash_filename \
+    --exclude '$RECYCLE.BIN/' \
+    --exclude 'System Volume Information' \
+    --exclude '.Trash-1000' \
+    "$source_dir" "./" | grep --color=always "deleting\|$" ;;
+    esac
