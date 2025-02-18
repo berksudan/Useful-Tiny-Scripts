53ce6304eb467d2711a10e0f2168cff40f850903
Berk Sudan <23002942+berksudan@users.noreply.github.com>
2025-02-18 16:42:45 +0100

    Update safe_git_add.sh

--- safe_git_add.sh
+++ safe_git_add.sh
@@ -1,10 +1,10 @@
-# Make sure you have installed mypy and ruff
+ # Make sure you have installed mypy and ruff
 git add .
 for file in $(git diff --name-only --cached -- '*.py'); do
     echo "$file"
     ruff check --fix "$file"
     ruff check --config pyproject.toml
     ruff format "$file"
-    mypy "$file"
+    echo "Mypy running.." && mypy "$file" && echo "Mypy finished.."
 done
 git add .
